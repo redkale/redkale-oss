9fc8f9183e77a959a2b696486541de7b7cecc6b7
redkale <redkale@qq.com>
2024-06-12 16:19:41 +0800

    RetCodes

--- src/main/java/org/redkale/oss/base/OssRetCodes.java
+++ src/main/java/org/redkale/oss/base/OssRetCodes.java
@@ -5,21 +5,18 @@
  */
 package org.redkale.oss.base;
 
-import java.text.MessageFormat;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import org.redkale.service.*;
 
 /**
  *
  * @author zhangjx
  */
-public abstract class OssRetCodes {
+public abstract class OssRetCodes extends RetCodes {
 
-    //2000_0001 - 2999_9999 预留给 Redkale的扩展包redkalex使用
-    //3000_0001 - 7999_9999 为平台系统使用
-    //8000_0001 - 9999_9999 为OSS系统使用
-    //------------------------------------- 通用模块 -----------------------------------------
+    // 2000_0001 - 2999_9999 预留给 Redkale的扩展包redkalex使用
+    // 3000_0001 - 7999_9999 为平台系统使用
+    // 8000_0001 - 9999_9999 为OSS系统使用
+    // ------------------------------------- 通用模块 -----------------------------------------
     @RetLabel("参数无效")
     public static final int RET_PARAMS_ILLEGAL = 30010001;
 
@@ -32,7 +29,7 @@ public abstract class OssRetCodes {
     @RetLabel("上传文件不是图片")
     public static final int RET_UPLOAD_NOTIMAGE = 30010004;
 
-    //------------------------------------- 用户模块 -----------------------------------------
+    // ------------------------------------- 用户模块 -----------------------------------------
     @RetLabel("未登录")
     public static final int RET_USER_UNLOGIN = 30020001;
 
@@ -109,13 +106,13 @@ public abstract class OssRetCodes {
     public static final int RET_USER_QQID_INFO_FAIL = 30020025;
 
     @RetLabel("验证码无效")
-    public static final int RET_USER_RANDCODE_ILLEGAL = 30020026; //邮件或者短信验证码
+    public static final int RET_USER_RANDCODE_ILLEGAL = 30020026; // 邮件或者短信验证码
 
     @RetLabel("验证码已过期")
-    public static final int RET_USER_RANDCODE_EXPIRED = 30020027; //邮件或者短信验证码
+    public static final int RET_USER_RANDCODE_EXPIRED = 30020027; // 邮件或者短信验证码
 
     @RetLabel("验证码错误或失效")
-    public static final int RET_USER_CAPTCHA_ILLEGAL = 30020028; //图片验证码
+    public static final int RET_USER_CAPTCHA_ILLEGAL = 30020028; // 图片验证码
 
     @RetLabel("用户类型无效")
     public static final int RET_USER_TYPE_ILLEGAL = 30020029;
@@ -123,76 +120,8 @@ public abstract class OssRetCodes {
     @RetLabel("用户设备ID无效")
     public static final int RET_USER_APPTOKEN_ILLEGAL = 30020030;
 
-    //-----------------------------------------------------------------------------------------------------------
-    protected static final Map<String, Map<Integer, String>> rets = RetLabel.RetLoader.loadMap(OssRetCodes.class);
-
-    protected static final Map<Integer, String> defret = rets.get("");
-
-    public static RetResult retResult(int retcode) {
-        if (retcode == 0) return RetResult.success();
-        return new RetResult(retcode, retInfo(retcode));
-    }
-
-    public static RetResult retResult(String locale, int retcode) {
-        if (retcode == 0) return RetResult.success();
-        return new RetResult(retcode, retInfo(locale, retcode));
-    }
-
-    public static RetResult retResult(int retcode, Object... args) {
-        if (retcode == 0) return RetResult.success();
-        if (args == null || args.length < 1) return new RetResult(retcode, retInfo(retcode));
-        String info = MessageFormat.format(retInfo(retcode), args);
-        return new RetResult(retcode, info);
-    }
-
-    public static RetResult retResult(String locale, int retcode, Object... args) {
-        if (retcode == 0) return RetResult.success();
-        if (args == null || args.length < 1) return new RetResult(retcode, retInfo(locale, retcode));
-        String info = MessageFormat.format(retInfo(locale, retcode), args);
-        return new RetResult(retcode, info);
-    }
-
-    public static <T> CompletableFuture<RetResult<T>> retResultFuture(int retcode) {
-        return CompletableFuture.completedFuture(retResult(retcode));
-    }
-
-    public static <T> CompletableFuture<RetResult<T>> retResultFuture(String locale, int retcode) {
-        return CompletableFuture.completedFuture(retResult(locale, retcode));
-    }
-
-    public static <T> CompletableFuture<RetResult<T>> retResultFuture(int retcode, Object... args) {
-        return CompletableFuture.completedFuture(retResult(retcode, args));
-    }
-
-    public static <T> CompletableFuture<RetResult<T>> retResultFuture(String locale, int retcode, Object... args) {
-        return CompletableFuture.completedFuture(retResult(locale, retcode, args));
-    }
-
-    public static RetResult set(RetResult result, int retcode, Object... args) {
-        if (retcode == 0) return result.retcode(0).retinfo("");
-        if (args == null || args.length < 1) return result.retcode(retcode).retinfo(retInfo(retcode));
-        String info = MessageFormat.format(retInfo(retcode), args);
-        return result.retcode(retcode).retinfo(info);
-    }
-
-    public static RetResult set(RetResult result, String locale, int retcode, Object... args) {
-        if (retcode == 0) return result.retcode(0).retinfo("");
-        if (args == null || args.length < 1) return result.retcode(retcode).retinfo(retInfo(locale, retcode));
-        String info = MessageFormat.format(retInfo(locale, retcode), args);
-        return result.retcode(retcode).retinfo(info);
-    }
-
-    public static String retInfo(int retcode) {
-        if (retcode == 0) return "Success";
-        return defret.getOrDefault(retcode, "Error");
-    }
-
-    public static String retInfo(String locale, int retcode) {
-        if (locale == null || locale.isEmpty()) return retInfo(retcode);
-        if (retcode == 0) return "Success";
-        String key = locale == null ? "" : locale;
-        Map<Integer, String> map = rets.get(key);
-        if (map == null) return "Error";
-        return map.getOrDefault(retcode, "Error");
+    // -----------------------------------------------------------------------------------------------------------
+    static {
+        load(OssRetCodes.class);
     }
 }
